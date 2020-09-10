63b2e28a5c4dbbec0290c435400d01f810fd9f92
kazuma.hosaka <kazumah32@gmail.com>
2020-09-10 12:06:20 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 
-<h1>タスク新規作成ページ</h1>
+<h1>タスク新規作成ページ</h1
+
+{!! Form::model($task, ['route' => 'tasks.store']) !!}
 
     <div class="row">
         <div class="col-6">
@@ -11,6 +13,11 @@
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('status', '状況:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
 
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
